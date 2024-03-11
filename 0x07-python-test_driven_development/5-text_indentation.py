1606869057f96d26ba5e7b0768eb8e4ba460d6e3
bwalya-305 <bwalyamwansa305@gmail.com>
2024-03-11 06:16:10 -0700

    made changes in code to correct output

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-"""
-Module text_indentation
-Adds two new lines after a set of characters.
-"""
-
 
 def text_indentation(text):
-    """Prints text with added two newlines
-    after each of these characters {'.', '?', ':'}.
     """
-
-    if type(text) is not str:
+    This function prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
+    
+    special_chars = ['.', '?', ':']
+    for char in special_chars:
+        text = text.replace(char, char + '\n\n')
+    
+    lines = text.split('\n')
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip()
+    
+    text = '\n'.join(lines)
+    print(text)
 
-    for delim in ".:?":
-        text = (delim + "\n\n").join(
-            [line.strip(" ") for line in text.split(delim)])
-
-    print("{}".format(text), end="")
